8a0bdc9e854eda3b2ca46c5d3b23d8e6139aeda1
QPS-Basavanagudi <QPS-Basavanagudi@DESKTOP-F52EDBA>
2022-12-01 19:08:08 +0530

    First commit

---
+++ src/Practice/TreeSetPractice.java
@@ -0,0 +1,16 @@
+package Practice;
+
+import java.util.TreeSet;
+
+public class TreeSetPractice {
+
+	public static void main(String[] args) {
+		TreeSet<Object> set = new TreeSet<>();
+		set.add("A");
+		set.add('C');
+		set.add(10);
+		set.add(20.02);
+		System.out.println(set);
+	}
+
+}
